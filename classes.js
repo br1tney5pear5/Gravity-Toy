0479f5f312aa383c6bb901411a0a3ac079302e86
Kacper Kokot <33990111+kacperkokot@users.noreply.github.com>
2017-12-23 11:09:50 +0100

    Time class added

--- classes.js
+++ classes.js
@@ -1,3 +1,17 @@
+class Time{
+	constructor(){
+		this.startTime = new Date().getTime(),	
+		this.lastTime = 0;
+		this.toggle = false;
+	}
+	getTime(){return Math.abs(this.startTime - new Date().getTime());}
+	getTimeInSeconds(){return Math.abs(this.startTime - new Date().getTime())/1000;}
+	deltaTime(){return Math.abs(this.lastTime - this.getTime());}
+	deltaTimeInSeconds(){return Math.abs(this.lastTime - this.getTime())/1000;}
+	setLastTime(){this.lastTime = this.getTime();}
+
+}
+
 class Vector2{
 	constructor(_x=0,_y=0){this.x = _x; this.y = _y;}
 	magnitude() {return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2));}
